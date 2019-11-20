ebcfbff14736be7213f2f7ac637d8552c07a07e9
etugoluk <chihyahya98@gmail.com>
2019-11-20 20:53:30 +0200

    parser done, solver WIP

---
+++ Parser.py
@@ -0,0 +1,136 @@
+import re
+
+RULE_REG = re.compile(r"^([A-Z\s\(\)\+\^\|\!]+)(<?=>)([A-Z\s\(\)\+\^\|\!]+)(?:#.*)?$")
+OUTPUT_REG = re.compile(r"^=\w+\s*(#.*)?$")
+QUERY_REG = re.compile(r"^\?\w+\s*(#.*)?$")
+TOKEN_REG = re.compile(r"!?\S")
+
+class ParserError(Exception):
+	def __init__(self, info):
+		super(ParserError, self).__init__("ParserError: " + info + '.')
+
+class LexicalError(Exception):
+	def __init__(self, info):
+		super(LexicalError, self).__init__("LexicalError: " + info + '.')
+
+def tokenize(rule):
+	return re.findall(TOKEN_REG, rule)
+
+def is_rule_side_valid(line):
+	left_br = 0
+	right_br = 0
+	fact = 0
+	sign = 0
+
+	for c in line:
+		if right_br > left_br:
+			raise ParserError('Invalid brackets')
+		elif c >= 'A' and c <= 'Z':
+			if fact == 1: #previous symbol was fact
+				return False
+			fact = 1
+			sign = 0
+		elif c in ('+|^'):
+			if sign == 1: #previous symbol was sign
+				return False
+			fact = 0
+			sign = 1
+		elif c == '!':
+			if fact == 1:
+				return False
+		elif c == '(':
+			if fact == 1:
+				return False
+			left_br += 1
+		elif c == ')':
+			if sign == 1:
+				return False
+			right_br += 1
+		else:
+			raise LexicalError('Unknown symbol %s' %c)
+
+	if (right_br != left_br):
+		raise ParserError('Invalid brackets')
+
+	return True
+
+def parse_rule(line, facts):
+	match = re.findall(RULE_REG, line)
+	if (len(match) == 0):
+		raise ParserError('Incorrect rule')
+	print (match)
+
+	rule = {}
+	rule['lhs'], rule['sign'], rule['rhs'] = match[0]
+	if is_rule_side_valid(rule['lhs']) == False or \
+		is_rule_side_valid(rule['rhs']) == False:
+		raise ParserError('Incorrect rule')
+	rule['lhs_value'], rule['rhs_value'] = -1, -1
+	print (rule)
+
+	for ch in rule['lhs']:
+		if (ch >= 'A') and (ch <= 'Z') and ch not in facts:
+			facts[ch] = 0
+
+	for ch in rule['rhs']:
+		if (ch >= 'A') and (ch <= 'Z'):
+			facts[ch] = -1
+
+	return rule
+
+def parse_output(line, facts):
+	for i in range(1, len(line)):
+		ch = line[i]
+		facts[ch] = 1
+
+def parse_query(line, facts):
+	query_list = []
+	for i in range(1, len(line)):
+		ch = line[i]
+		if ch not in facts:
+			raise LexicalError("query not found: %s" %ch)
+		query_list.append(ch)
+	return query_list
+
+def parse_file(file):
+	facts = {}
+	rules = []
+	queries = []
+
+	output_flag = 0
+	query_flag = 0
+	try:
+		for i, line in enumerate(file):
+			line = re.sub('#.*', '', line)
+			line = re.sub('\\s*', '', line)
+			if len(line) < 2:
+				continue
+			elif len(RULE_REG.findall(line)) > 0:
+				if output_flag:
+					raise ParserError("rule after output")
+				if query_flag:
+					raise ParserError("rule after query")
+				rule = parse_rule(line, facts)
+				rules.append(rule)
+			elif len(OUTPUT_REG.findall(line)) > 0:
+				if query_flag:
+					raise ParserError("output after query")
+				parse_output(line, facts)
+				output_flag = 1
+			elif len(QUERY_REG.findall(line)) > 0:
+				if query_flag:
+					raise ParserError("multiple query")
+				queries = parse_query(line, facts)
+				query_flag = 1
+			else:
+				raise ParserError('incorrect input on line %d' %(i+1))
+
+		if output_flag == 0:
+			raise ParserError("no output expression")
+		if query_flag == 0:
+			raise ParserError("no query expression")
+	except Exception as e:
+		print (e)
+		exit (0)
+
+	return facts, rules, queries
--- expert_system.py
+++ expert_system.py
@@ -1,51 +1,210 @@
 import argparse
-import re
+import Parser
 
-def parse_rule(line):
-	line = re.sub('#.*', '', line)
-	line = re.sub('\\s*', '', line)
-	print (line)
-	lhs, rhs = re.compile("<=>|=>").split(line)
-	pass
+#FOR TEST
+import os
 
-def parse_output(line):
-	line = re.sub('#.*', '', line)
-	line = re.sub('\\s*', '', line)
+# Symbols dictionary values:
+# 1 symbol=True
+# 0 symbol=False
+#-1 symbol=Undefined
 
-	for i in range(1, len(line)):
-		ch = line[i]
-		symbols_dict[ch] = True
+# def check_rule(rule):
+# 	lhs_value = compute(rule['lhs'])
+# 	rhs_value = compute(rule['rhs'])
 
-def parse_query(line):
-	line = re.sub('#.*', '', line)
-	line = re.sub('\\s*', '', line)
+# 	if lhs_value == True and rhs_value == False:
+# 		return False
+# 	if lhs_value == False and rhs_value == True and rule['sign'] == '<=>':
+# 		return False
 
-	for i in range(1, len(line)):
-		ch = line[i]
-		query_list.append(ch)
+# 	return True
 
-parser = argparse.ArgumentParser()
-parser.add_argument('file', help='input file')
-args = parser.parse_args()
+# def check(fact_rules):
+# 	for rule in fact_rules:
+# 		if (check_rule(rule) == False):
+# 			return False
+# 	return True
 
-comment_reg = re.compile("^\\s*#.*")
-rule_reg = re.compile("^!?\\w ((\\+|\\||\\^)( !?\\w ))*(=>|<=>) !?\\w( ((\\+|\\||\\^)( !?\\w ))*)*(#.*)?")
-output_reg = re.compile("^=\\w+\\s*#.*")
-query_reg = re.compile("^\?\\w+\\s*#.*")
-symbols_dict = {}
-query_list = []
 
-f = open(args.file, 'r')
-for i, line in enumerate(f):
-	if len(line) < 2 or len(comment_reg.findall(line)) > 0:
-		continue
-	elif len(rule_reg.findall(line)) > 0:
-		parse_rule(line)
-	elif len(output_reg.findall(line)) > 0:
-		parse_output(line)
-	elif len(query_reg.findall(line)) > 0:
-		parse_query(line)
+def evaluate(x1, x2, op):
+	if (x1 == -1 or x2 == -1):
+		if (op == '|' and (x1 == 1 or x2 == 1)):
+			return 1
+		if (op == '+' and (x1 == 0 or x2 == 0)):
+			return 0
+		return -1
+
+	if op == '+':
+		res = x1 & x2
+	elif op == '|':
+		res = x1 | x2
+	elif op == '^':
+		res = x1 ^ x2
+
+	return res
+
+def is_var(token):
+	offs = int(token[0] == '!')
+	return token[offs:].isalpha()
+
+def is_op(token):
+	return token in '+|^'
+
+def get_priority(op):
+	return '^|+'.index(op)
+
+def to_rpn(tokens):
+	out_queue = []
+	op_stack = []
+	for token in tokens:
+		if is_var(token):
+			out_queue.append(token)
+		elif is_op(token):
+			for op in list(op_stack[::-1]):
+				if get_priority(op_stack[-1]) > get_priority(token):
+					out_queue.append(op_stack[-1])
+					del op_stack[-1]
+				else:
+					break
+			op_stack.append(token)
+		elif token == '(':
+			op_stack.append(token)
+		elif token == ')':
+			for op in list(op_stack[::-1]):
+				if op == '(':
+					del op_stack[-1]
+					break
+				else:
+					out_queue.append(op)
+					del op_stack[-1]
+	out_queue += op_stack[::-1]
+	return out_queue
+
+def get_val(key):
+	if key[0] == '!':
+		val = facts[key[1]]
+		if val != -1:
+			val ^= 1
 	else:
-		print ('Line %d: incorrect input' %(i+1))
-	
-f.close()
+		val = facts[key]
+	return val
+
+def compute(rule):
+	if len(rule) < 3:
+		return get_val(rule)
+
+	op_queue = to_rpn(Parser.tokenize(rule))
+	operands = []
+	result = None
+
+	for elem in op_queue:
+		if is_op(elem):
+			x1 = operands.pop()
+			x2 = operands.pop()
+			result = evaluate(x1, x2, elem)
+			operands.append(result)
+		else:
+			operands.append(get_val(elem))
+
+	return result
+
+def get_fact_rules(fact, rules):
+	fact_rules = []
+	for r in rules:
+		if (fact in r['rhs']) or ((ch in r['lhs'] and r['sign'] == '<=>')):
+			fact_rules.append(r)
+
+	return fact_rules
+
+def get_fact(ch, stack):
+	#check if symbol is known already
+	if facts[ch] != -1:
+		return facts[ch]
+
+	#check for recursive
+	if ch in stack:
+		return -1
+	stack.insert(0, ch)
+
+	#find all rhs in rules with symbol
+	fact_rules = get_fact_rules(ch, rules_list)
+
+	#if no rules for symbol then it is false by default
+	if len(fact_rules) == 0:
+		return -1
+
+	for f_rule in fact_rules:
+		lhs = 'lhs' if ch in f_rule['rhs'] else 'rhs' #TODO: process case when variable in two sides
+		rhs = 'rhs' if ch in f_rule['rhs'] else 'lhs'
+
+		for c in f_rule[lhs]:
+			if (c >= 'A') and (c <= 'Z'):
+				facts[c] = get_fact(c, stack)
+
+		lhs_value = compute(f_rule[lhs])
+		if lhs_value == -1:
+			continue
+
+		# rules_list[lhs_value] = lhs_value
+		f_rule[lhs_value] = lhs_value
+
+		if f_rule['sign'] == '<=>':
+			# rules_list['rhs_value'] = lhs_value
+			f_rule['rhs_value'] = lhs_value
+		elif f_rule['sign'] == '=>' and lhs_value == 1:
+			# rules_list['rhs_value'] = lhs_value
+			f_rule['rhs_value'] = 1
+
+		if f_rule['rhs_value'] == -1: #invalid rule
+			continue
+
+		if len(f_rule[rhs]) < 3: #if only element in rhs
+			if f_rule[rhs][0] == '!':
+				facts[ch] = not f_rule['rhs_value']
+			else:
+				facts[ch] = f_rule['rhs_value']
+			return facts[ch]
+		else:
+			for c in f_rule[rhs]:
+				if (c >= 'A') and (c <= 'Z') and (c != ch):
+					facts[c] = get_fact(c, stack)
+
+			#pretend symbol = false
+			facts[ch] = 0
+			ch_false = compute(f_rule[rhs])
+
+			facts[ch] = 1
+			ch_true = compute(f_rule[rhs])
+
+			if ch_true == ch_false or \
+				ch_true == -1 or ch_false == -1:
+				facts[ch] = -1
+				continue
+			elif (ch_false == f_rule['rhs_value']):
+				facts[ch] = 0
+				return 0
+			elif (ch_true == f_rule['rhs_value']):
+				facts[ch] = 1
+				return 1
+
+	return facts[ch]
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('file', help='input file')
+	args = parser.parse_args()
+
+	file = open(args.file, 'r')
+	facts, rules_list, queries = Parser.parse_file(file)
+	file.close()
+
+	for ch in queries:
+		stack = []
+		res = get_fact(ch, stack)
+		if res == 1:
+			print ("%c is True" %ch)
+		elif res == 0:
+			print ("%c is False" %ch)
+		else:
+			print ("%c is undefined" %ch)
